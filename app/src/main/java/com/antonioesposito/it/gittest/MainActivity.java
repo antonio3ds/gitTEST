9526414a3239c8dc7089d2bf6249cfb5f76d0bd5
antonioesposito <antonio3ds@gmail.com>
2017-02-27 17:29:52 +0100

    Primo commit dev 1

--- app/src/main/java/com/antonioesposito/it/gittest/MainActivity.java
+++ app/src/main/java/com/antonioesposito/it/gittest/MainActivity.java
@@ -2,12 +2,22 @@ package com.antonioesposito.it.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button mybutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mybutton= findViewById("mybutton");
+
+
+       //  mybutton.setOnClickListener(new );
     }
 }
